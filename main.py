e22ee86e95320d0a28be8da386c78d3d3394f8eb
MyDogLovesGitHub <44881955+MyDogLovesGitHub@users.noreply.github.com>
2024-07-31 23:00:32 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import pyautogui, random
+from time import sleep
+
+# INCREATE ESTIMATED PING IF YOUR INTERNET IS SLOW
+estimatedPing = 30 # Estimated ping (Just in case of lag)
+delay = 5+(estimatedPing/1000) # Delay between messages (5+a bit bc of slow mode)
+
+with open('messages.txt', 'r') as f:
+    messages = [n.strip() for n in f.readlines()]
+
+
+def Countdown() -> None:
+    # Gives you 5 seconds to switch to twitch and click on the "Send a message" bar
+
+    for i in range(5):
+        print(5-i)
+        sleep(1)
+    print('Go')
+
+def Main() -> None:
+    # The thing that types
+    messageIndex = 0
+
+    while True:
+        pyautogui.write(messages[messageIndex])
+        pyautogui.press('enter')
+
+        messageIndex = (messageIndex+1) if messageIndex<len(messages) else 0
+        sleep(delay)
+
+Countdown()
+Main()
+input('Script Finished (press enter to close)')
+
